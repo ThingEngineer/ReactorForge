340979ae3e56b0b5226d8a4741ad1082bc88d82d
ThingEngineer <josh.lee.campbell@gmail.com>
2017-12-15 14:17:28 -0600

    test code

--- firmware/CriticalMass/src/main.cpp
+++ firmware/CriticalMass/src/main.cpp
@@ -1,9 +1,30 @@
-#include <Arduino.h>
+//https://atom.io/packages/platomformio
+
+#ifndef F_CPU
+#define F_CPU 16000000UL // 16 MHz clock speed
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
 
 void setup() {
-    // put your setup code here, to run once:
+  //Set port PORTD data direction registers to output
+  DDRD = 0xFF;
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+int main(void)
+{
+  setup();
+
+  while(1)
+  {
+    // Set all of PORTD to high
+    PORTD = 0xFF;
+    //1 second delay
+    _delay_ms(1000);
+    // Set all of PORTD to low
+    PORTD= 0x00;
+    //1 second delay
+    _delay_ms(1000);
+  }
 }
