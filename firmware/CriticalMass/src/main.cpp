0cb503c070ac7783dc4d2d9928f8454ba99f018e
ThingEngineer <josh.lee.campbell@gmail.com>
2017-12-15 15:03:49 -0600

    change variable type

--- firmware/CriticalMass/src/main.cpp
+++ firmware/CriticalMass/src/main.cpp
@@ -1,7 +1,7 @@
 //https://atom.io/packages/platomformio
 
 #ifndef F_CPU
-#define F_CPU 16000000UL // 16 MHz clock speed
+#define F_CPU 16000000L // 16 MHz clock speed
 #endif
 
 #include <avr/io.h>
