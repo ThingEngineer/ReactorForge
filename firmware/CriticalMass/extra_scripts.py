16bae3d642bc731da4f2aa24ba1acf930d12ed26
ThingEngineer <josh.lee.campbell@gmail.com>
2017-12-16 19:20:33 -0600

    added another fuse calculator

--- firmware/CriticalMass/extra_scripts.py
+++ firmware/CriticalMass/extra_scripts.py
@@ -1,5 +1,6 @@
 Import('env')
 
 #Override fuses set in board manifest
+#http://www.engbedded.com/fusecalc/
 #http://eleccelerator.com/fusecalc/fusecalc.php?chip=at90pwm316&LOW=F3&HIGH=DE&EXTENDED=C9&LOCKBIT=FF
 #env.Replace(FUSESCMD="avrdude $UPLOADERFLAGS -e -Ulfuse:w:0xF3:m -Uhfuse:w:0xDE:m -Uefuse:w:0xC9:m -Ulock:w:0xFF:m")
